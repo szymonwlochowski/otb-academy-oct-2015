3699e5010e93033355f01f046ee3289e659feb4a
Szymon Wlochowski <szymonwlochowski@Szymons-MacBook-Pro.local>
2015-09-21 11:38:16 +0100

    rspec passing

--- the-language/spec/introduction_spec.rb
+++ the-language/spec/introduction_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "Learning ruby" do
   context "using expectations" do
 
     it "checks reality" do
-      expect(true).to be(true)
+      expect( true ).to be true # it should be true
     end
 
     it "verifies our expectations against reality" do
@@ -21,7 +21,7 @@ RSpec.describe "Learning ruby" do
     end
 
     it "will ask you to fill in values" do
-      expect(1 + 1).to eq(2)
+      expect( 1 + 1 ).to eq 2
     end
 
   end
