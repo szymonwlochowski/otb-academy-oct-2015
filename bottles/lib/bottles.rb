2a01d0da8bef80afc292d97f6c785b1039bc860e
Szymon Wlochowski <szymonwlochowski@gmail.com>
2015-09-28 11:35:28 +0100

    Almost there. Just one more line repeated

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,15 +1,30 @@
 class Bottles
 	def verse(num)
-		if num > 2
-			"#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottles of beer on the wall.\n"
-    elsif num == 2
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottle of beer on the wall.\n"
-    elsif num == 1
-      "#{num} bottle of beer on the wall, #{num} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    
+		if num >= 1
+			"#{bottle_or_bottles(num)} of beer on the wall, #{bottle_or_bottles(num)} of beer.\nTake #{one_or_it(num)} down and pass it around, #{bottle_or_bottles(num-1)} of beer on the wall.\n"
     else
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
 	end
+
+  def bottle_or_bottles(num)
+    if num == 1
+      "#{num} bottle"
+    elsif num > 1
+      "#{num} bottles"
+    else
+      "no more bottles"
+    end
+  end
+
+  def one_or_it(num)
+    if num > 1
+      "one"
+    else
+      "it"
+    end
+  end
 
   def verses(num, num2)
     result = ""
