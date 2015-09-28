4b50f81a7e163ec7c2bf63684a23a486bd3efc38
Szymon Wlochowski <szymonwlochowski@gmail.com>
2015-09-28 12:38:03 +0100

    refactor

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,33 +1,25 @@
 class Bottles
 	def verse(num)
-    "#{bottle_or_bottles(num)} of beer on the wall, #{bottle_or_bottles(num)} of beer.\n".capitalize+"#{one_or_more(num)}".capitalize
+    #"#{bottle_or_bottles(num)} of beer on the wall, #{bottle_or_bottles(num)} of beer.\n".capitalize+"#{one_or_more(num)}".capitalize
+    "#{bottle_or_bottles(num)} on the wall, #{bottle_or_bottles(num)}.\n#{one_or_more(num)}".gsub(/^\W*./, &:upcase)
   end
 
   def bottle_or_bottles(num)
     if num == 1
-      "#{num} bottle"
+      "#{num} bottle of beer"
     elsif num > 1
-      "#{num} bottles"
+      "#{num} bottles of beer"
     else
-      "no more bottles"
+      "no more bottles of beer"
     end
   end
 
   def one_or_it(num)
-    if num > 1
-      "one"
-    else
-      "it"
-    end
+    num > 1 ? "one" : "it"
   end
 
   def one_or_more(num)
-    if num >= 1
-      "Take #{one_or_it(num)} down and pass it around, #{bottle_or_bottles(num-1)} of beer on the wall.\n"
-    else
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n".capitalize
-  end
-    
+    num >= 1 ? "Take #{one_or_it(num)} down and pass it around, #{bottle_or_bottles(num-1)} on the wall.\n" : "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 
   def verses(num, num2)
