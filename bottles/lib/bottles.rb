ee775795365447f46ff3af091f2e57ad32392d12
Szymon Wlochowski <szymonwlochowski@gmail.com>
2015-09-28 14:11:29 +0100

    Sean's solution to be refactored

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
 	def verse(num)
     #"#{bottle_or_bottles(num)} of beer on the wall, #{bottle_or_bottles(num)} of beer.\n".capitalize+"#{one_or_more(num)}".capitalize
-    "#{bottle_or_bottles(num)} on the wall, #{bottle_or_bottles(num)}.\n#{one_or_more(num)}".gsub(/^\W*./, &:upcase)
+    "#{bottle_or_bottles(num)} on the wall, #{bottle_or_bottles(num)}.#{one_or_more(num)}".gsub(/^\W*./, &:upcase)
   end
 
   def bottle_or_bottles(num)
@@ -19,7 +19,7 @@ class Bottles
   end
 
   def one_or_more(num)
-    num >= 1 ? "Take #{one_or_it(num)} down and pass it around, #{bottle_or_bottles(num-1)} on the wall.\n" : "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    num >= 1 ? "\nTake #{one_or_it(num)} down and pass it around, #{bottle_or_bottles(num-1)} on the wall.\n" : "\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 
   def verses(num, num2)
