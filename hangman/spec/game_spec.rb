30f58fbb60826b0a0451b4813130f96b56dffab7
Szymon Wlochowski <szymonwlochowski@gmail.com>
2015-09-29 16:30:43 +0100

    State as of the end of Tuesday. In the process of writing the tests.

---
+++ hangman/spec/game_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe "Game" do
+  let(:generator) { double("generator", :generate_word => "rabbit")}
+  let(:display) { double("display")}
+  let(:game) { Game.new(generator, display) }
+
+  context "when initialized" do
+
+    it "generates a word" do
+      expect( generator ).to receive(:generate_word)
+      game = Game.new(generator, display)
+    end
+
+    it "displays state of the game" do
+      expect( display ).to receive(:display_word).with("______")
+      expect( display ).to receive(:display_scaffold).with(0)
+      expect( display ).to receive(:display_bad_guesses).with("")
+      game = Game.new(generator, display)
+    end
+
+  end
+
+  context "when playing the game" do
+
+    it "handles incorrect guess" do
+      expect( display ).to receive(:display_scaffold).with(1)
+      expect( display ).to receive(:display_bad_guesses).with("e")
+      game.guess("e")
+    end
+
+    it "handles correct guess" do
+      expect( display ).to receive(:display_word).with("r_____")
+      game.guess("r")
+    end
+
+    it "handles correct guess with multiple hits" do
+      expect( display ).to receive(:display_word).with("__bb__")
+      game.guess("b")
+    end
+
+  end
+
+  context "at the end of the game" do
+
+    it "handles winning correctly" do
+      expect( display ).to receive(:display_end_game).with("you won")
+    end
+
+  end
+
+end
